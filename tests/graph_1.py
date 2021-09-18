3f25d33041827374c5d2e41008871efb9d299b18
Денис Козуб <kozubtv@hotmail.com>
2021-09-18 18:39:46 +0300

    timer replace

--- tests/graph_1.py
+++ tests/graph_1.py
@@ -4,21 +4,21 @@ from visibility.visibility_graph import VisibilityGraph
 
 
 def main():
-    start = timeit.default_timer()
-
     vgraph = VisibilityGraph()
     vgraph.load_geometry("../maps/user_area.h5")
 
-    map_plot = ('r', {0: "royalblue", 1: "r", 2: "k"})
+    start = timeit.default_timer()
+
     G, fig = vgraph.build_graph(inside_percent=0,
                                 multiprocessing=False,
                                 graph=True,
-                                map_plot=map_plot)
+                                map_plot=None)
+
+    stop = timeit.default_timer()
 
     print('edges: ', G.number_of_edges())
     print('nodes: ', G.number_of_nodes())
 
-    stop = timeit.default_timer()
     print('Time: ', stop - start)
 
 
--- tests/graph_2.py
+++ tests/graph_2.py
@@ -6,22 +6,22 @@ from visibility.visibility_graph import VisibilityGraph
 
 
 def main():
-    start = timeit.default_timer()
-
     vgraph = VisibilityGraph()
     vgraph.load_geometry("../maps/kozlovo.h5")
 
-    map_plot = ('r', {0: "royalblue", 1: "r", 2: "k"})
+    start = timeit.default_timer()
+
     with cProfile.Profile() as pr:
         G, fig = vgraph.build_graph(inside_percent=0,
                                     multiprocessing=False,
                                     graph=True,
-                                    map_plot=map_plot)
+                                    map_plot=None)
+
+    stop = timeit.default_timer()
 
     print('edges: ', G.number_of_edges())
     print('nodes: ', G.number_of_nodes())
 
-    stop = timeit.default_timer()
     print('Time: ', stop - start)
 
     stats = pstats.Stats(pr)
