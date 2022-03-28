2ff31f6e8b4e4ea97dd5869ad5a40e643723c778
Denikozub <denikozub@gmail.com>
2022-03-28 20:06:13 +0300

    pandas append to concat

--- src/offroad_routing/osm_data/parser.py
+++ src/offroad_routing/osm_data/parser.py
@@ -3,12 +3,13 @@ from math import fabs
 from geopandas import GeoDataFrame
 from offroad_routing.osm_data.osm_converter import OsmConverter
 from offroad_routing.osm_data.tag_value import TagValue
+from pandas import concat
+from pandas import DataFrame
 from pyrosm import OSM
 from shapely.geometry import mapping
 
 
 class OsmParser:
-
     __slots__ = ("polygons", "multilinestrings", "tag_value", "bbox_size")
 
     def __init__(self):
@@ -72,28 +73,28 @@ class OsmParser:
         if natural is not None:
             natural = natural.loc[:, ['natural', 'geometry']].rename(
                 columns={'natural': 'tag'})
-            self.polygons = self.polygons.append(
-                natural.loc[natural.geometry.type == 'Polygon'])
-            multipolygons = multipolygons.append(
-                natural.loc[natural.geometry.type == 'MultiPolygon'])
+            self.polygons = concat([self.polygons,
+                                    natural.loc[natural.geometry.type == 'Polygon']])
+            multipolygons = concat([multipolygons,
+                                    natural.loc[natural.geometry.type == 'MultiPolygon']])
             natural.drop(natural.index, inplace=True)
 
         landuse = osm.get_landuse()
         if landuse is not None:
             landuse = landuse.loc[:, ['landuse', 'geometry']].rename(
                 columns={'landuse': 'tag'})
-            self.polygons = self.polygons.append(
-                landuse.loc[landuse.geometry.type == 'Polygon'])
-            multipolygons = multipolygons.append(
-                landuse.loc[landuse.geometry.type == 'MultiPolygon'])
+            self.polygons = concat([self.polygons,
+                                    landuse.loc[landuse.geometry.type == 'Polygon']])
+            multipolygons = concat([multipolygons,
+                                    landuse.loc[landuse.geometry.type == 'MultiPolygon']])
             landuse.drop(landuse.index, inplace=True)
 
         # splitting multipolygons to polygons
         for i in range(multipolygons.shape[0]):
             tag = multipolygons.tag.iloc[i]
             for polygon in multipolygons.geometry.iloc[i].geoms:
-                self.polygons = self.polygons.append(
-                    {'tag': tag, 'geometry': polygon}, ignore_index=True)
+                self.polygons = concat([self.polygons,
+                                        DataFrame({'tag': tag, 'geometry': polygon}, index=[0])], ignore_index=True)
 
         roads = osm.get_network()
         if roads is not None:
