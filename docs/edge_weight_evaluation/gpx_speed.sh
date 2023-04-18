945c6b7537fe0856ee4a9fccb946a9abff0dda14
Denis Kozub <denikozub@gmail.com>
2023-04-18 23:34:29 +0300

    edge weight evaluation

---
+++ docs/edge_weight_evaluation/gpx_speed.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# if required:
+# chmod +x gpx_speed.sh
+# sed -i -e 's/\r$//' gpx_speed.sh
+
+read folder
+
+rename -f 'y/A-Z/a-z/' ./$folder/*
+rename -f 'y/( |\-)/_/' ./$folder/*
+
+for f in ./$folder/*.gpx; do gpsbabel -t -i gpx -f $f -x track,speed -o gpx,gpxver=1.0 -F speed_${f##*/}; done
